416fcedcfcb64398c905f173f8fe13e697415753
JersonOchoa <ochoarayo@hotmail.com>
2018-09-26 17:49:34 -0600

    Agregar primer pagina y vistas

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezon
+from .models import Publicar
 
+def listar(request):
+    articulos = Publicar.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+    return render(request, 'blog/listar.html', {'articulos', articulos})
 # Create your views here.
