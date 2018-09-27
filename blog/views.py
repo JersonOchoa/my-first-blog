0b2076bc425c70f53d05ad2738307515a731ba62
JersonOchoa <ochoarayo@hotmail.com>
2018-09-26 18:17:18 -0600

    Se arreglo error de vistas

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,5 @@ from .models import Post
 
 def listar(request):
     articulos = Post.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
-    return render(request, 'blog/listar.html', {'articulos', articulos})
+    return render(request, 'blog/listar.html', {'articulos': articulos})
 # Create your views here.
