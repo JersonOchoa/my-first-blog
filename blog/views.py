101d1ccbba848a3097e2403c5f76f2fd15e207d0
JersonOchoa <ochoarayo@hotmail.com>
2018-10-03 18:06:48 -0600

    Se agrego vistas de detalle

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 def listar(request):
     articulos = Post.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/listar.html', {'articulos': articulos})
-# Create your views here.
+
+def detalle(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/detalle.html', {'post': post})
