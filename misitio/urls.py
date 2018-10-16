93abced22230930f4f86e4404dbaeb5ad53849d4
JersonOchoa <ochoarayo@hotmail.com>
2018-10-16 09:55:20 -0600

    Se agrego login y logout y se arregló un error en urls

--- misitio/urls.py
+++ misitio/urls.py
@@ -25,7 +25,6 @@ from django.contrib.auth.views import LoginView
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'', include('blog.urls')),
-    """url(r'^accounts/login/$', views.login, name='login'),"""
     url(r'^accounts/login/$', views.LoginView.as_view(), name='login'),
     url(r'^accounts/logout/$', views.LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
 ]
